42ca23d75d3315aa444938cc572402065952b061
rmcmurrer81 <robertmcmurrer@gmail.com>
2026-08-05 20:42:50 -0400

    Test Bell County Comic Con recognition and typo correction

--- Sarah_Morgan_Android_Phone_First_v3/tests/EventTripIntentParserTest.java
+++ Sarah_Morgan_Android_Phone_First_v3/tests/EventTripIntentParserTest.java
@@ -6,20 +6,28 @@ public final class EventTripIntentParserTest {
                 "I am going to Vegas for CES");
         require(ces.found(), "CES event must be found");
         require("CES".equals(ces.eventName), "CES name must be canonical");
-        require("Las Vegas".equals(ces.destination), "CES destination must be Las Vegas");
+        require(ces.destination.toLowerCase().contains("las vegas"), "CES destination must be Las Vegas");
         require(ces.monitoringRequested, "CES should create a monitor");
 
         EventTripIntentParser.EventIntent comicCon = EventTripIntentParser.parse(
                 "I am going to San Diego for comic con");
         require(comicCon.found(), "Comic-Con event must be found");
         require(comicCon.eventName.toLowerCase().contains("comic"), "Comic-Con name must be retained");
-        require("San Diego".equals(comicCon.destination), "Comic-Con destination must be San Diego");
+        require(comicCon.destination.toLowerCase().contains("san diego"), "Comic-Con destination must be San Diego");
 
         EventTripIntentParser.EventIntent generic = EventTripIntentParser.parse(
                 "I am traveling to Austin for the Future of Travel Conference");
         require(generic.found(), "generic named event must be found");
         require("Austin".equals(generic.destination), "generic event city must be Austin");
         require(generic.eventName.toLowerCase().contains("future of travel"), "generic event name must be retained");
+
+        EventTripIntentParser.EventIntent bellTypo = EventTripIntentParser.parse(
+                "I am thinking about going to bell country comic con");
+        require(bellTypo.found(), "Bell Country typo must still resolve the event");
+        require("Bell County Comic Con".equals(bellTypo.eventName),
+                "the event name must be corrected to Bell County Comic Con");
+        require("Belton, Texas".equals(bellTypo.destination),
+                "Bell County Comic Con must resolve to Belton, Texas");
 
         System.out.println("EventTripIntentParserTest passed");
     }
