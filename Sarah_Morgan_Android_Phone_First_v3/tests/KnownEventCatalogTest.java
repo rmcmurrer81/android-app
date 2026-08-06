1a0722a702cda3ba261f486d883642ebd512a49f
rmcmurrer81 <robertmcmurrer@gmail.com>
2026-08-05 21:30:52 -0400

    Test Indy PopCon official event resolution

--- Sarah_Morgan_Android_Phone_First_v3/tests/KnownEventCatalogTest.java
+++ Sarah_Morgan_Android_Phone_First_v3/tests/KnownEventCatalogTest.java
@@ -8,6 +8,12 @@ public final class KnownEventCatalogTest {
         require("Belton, Texas".equals(bell.destination), "event must resolve to Belton, Texas");
         require(bell.officialUrl.contains("bellcountycomiccon.com"), "official URL must be stored");
 
+        KnownEventCatalog.Entry indy = KnownEventCatalog.find("I am thinking about going to indy pop con");
+        require(indy != null, "Indy Pop Con wording must resolve");
+        require("PopCon Indy".equals(indy.eventName), "official PopCon name must be used");
+        require("Indianapolis, Indiana".equals(indy.destination), "PopCon must resolve to Indianapolis");
+        require(indy.officialUrl.contains("popcon.us/popcon-indy"), "PopCon official URL must be stored");
+
         KnownEventCatalog.Entry unknown = KnownEventCatalog.find("an imaginary convention");
         require(unknown == null, "unknown events must not be forced into the official catalog");
 
